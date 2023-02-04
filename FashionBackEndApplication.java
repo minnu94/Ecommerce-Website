010d00dd303b949789f6b842a2784467a189b0f3
minnu94 <124430940+minnu94@users.noreply.github.com>
2023-02-04 15:36:34 +0530

    Add files via upload

---
+++ FashionBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.fashionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionBackEndApplication.class, args);
+	}
+
+}
